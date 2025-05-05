25338fa266e34d7f3ed79c2617dbce4654e1db52
user1 <user01@em/com>
2025-05-05 10:13:36 +0300

    commit 1

---
+++ main.js
@@ -0,0 +1,74 @@
+
+// for (let i = 0; i < 25; i++) {
+
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(Math.pow(i, 2))
+// }
+
+// for (let i = 1; i < 20; i++) {
+//     if(i % 2 == 1){
+//         console.log(i)
+//     }
+// }
+
+//-------------------------
+
+
+// let sum = 0;
+
+// let n1 = 0;
+// let n2 = 1;
+// let nextTerm = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(n1);
+
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+
+//------------------------------------
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(Math.pow(i, 2))
+// }
+
+//------------------------------------
+
+let result = 0;
+
+for (let i = 1; i <= 10; i++){
+
+    result = 1 * i;
+
+    console.log(`${1} X ${i} = ${result}`);
+
+}
+
+result = 0;
+
+for (let i = 1; i <= 10; i++){
+
+    result = 2 * i;
+
+    console.log(`${2} X ${i} = ${result}`);
+
+}
+
+result = 0;
+
+for (let i = 1; i <= 10; i++){
+
+    result = 3 * i;
+
+    console.log(`${3} X ${i} = ${result}`);
+
+}
