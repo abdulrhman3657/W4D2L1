10cd96049aaa9cc1768c8b693fd7866bf3b5fb65
user1 <user01@em/com>
2025-05-05 10:15:08 +0300

    c2

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 
-// for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 25; i++) {
 
-//     if(i % 2 == 0){
-//         console.log(i)
-//     }
+    if(i % 2 == 0){
+        console.log(i)
+    }
 
-// }
+}
 
-// for (let i = 1; i < 10; i++) {
-//     console.log(Math.pow(i, 2))
-// }
+for (let i = 1; i < 10; i++) {
+    console.log(Math.pow(i, 2))
+}
 
-// for (let i = 1; i < 20; i++) {
-//     if(i % 2 == 1){
-//         console.log(i)
-//     }
-// }
+for (let i = 1; i < 20; i++) {
+    if(i % 2 == 1){
+        console.log(i)
+    }
+}
 
 //-------------------------
 
 
-// let sum = 0;
+let sum = 0;
 
-// let n1 = 0;
-// let n2 = 1;
-// let nextTerm = 0;
+let n1 = 0;
+let n2 = 1;
+let nextTerm = 0;
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(n1);
+for (let i = 1; i <= 10; i++) {
+    console.log(n1);
 
-//     nextTerm = n1 + n2;
-//     n1 = n2;
-//     n2 = nextTerm;
-// }
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
 
 
 //------------------------------------
 
-// for (let i = 1; i < 10; i++) {
-//     console.log(Math.pow(i, 2))
-// }
+for (let i = 1; i < 10; i++) {
+    console.log(Math.pow(i, 2))
+}
 
 //------------------------------------
 
